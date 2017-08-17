44a8811fc0f695cbd36a2ad7c30861a9d1e3ea1c
PhaserArray <phaserarrayksp@gmail.com>
2017-08-18 00:59:59 +0300

    Switched from checking inventories on Update() to checking them OnPlayerConnected(). Restricted items now get removed on pickup.

--- RestrictedItems.cs
+++ RestrictedItems.cs
@@ -12,21 +12,19 @@ using Rocket.Unturned.Events;
 using Rocket.Unturned.Permissions;
 using System;
 using Rocket.Unturned;
+using System.Threading;
+using Rocket.Unturned.Enumerations;
 
 namespace PhaserArray.RestrictedItems
 {
 	public class RestrictedItems : RocketPlugin<RestrictedItemsConfiguration>
 	{
 		private static RestrictedItemsConfiguration Config;
-		private float LastCheck;
-		private HashSet<UnturnedPlayer> CheckQueue;
 		private Dictionary<ushort, List<List<string>>> AllRestrictedItems;
 
 		protected override void Load()
 		{
 			Config = Configuration.Instance;
-			LastCheck = Time.time;
-			CheckQueue = new HashSet<UnturnedPlayer>();
 			AllRestrictedItems = new Dictionary<ushort, List<List<string>>>();
 
 			// Loads the config into a nicely indexable lowercase dictionary.
@@ -53,172 +51,186 @@ namespace PhaserArray.RestrictedItems
 
 			U.Events.OnPlayerConnected += OnPlayerConnected;
 			UnturnedPlayerEvents.OnPlayerInventoryAdded += OnPlayerInventoryAdded;
-			U.Events.OnPlayerDisconnected += OnPlayerDisconnected;
+			UnturnedPlayerEvents.OnPlayerWear += OnPlayerWear;
 
 			Logger.Log("Loaded " + AllRestrictedItems.Count.ToString() + " restricted items!");
 			Logger.Log("Plugin Loaded!");
 		}
-
+		
 		protected override void Unload()
 		{
 			U.Events.OnPlayerConnected -= OnPlayerConnected;
 			UnturnedPlayerEvents.OnPlayerInventoryAdded -= OnPlayerInventoryAdded;
-			U.Events.OnPlayerDisconnected -= OnPlayerDisconnected;
-		}
-
-		public void QueuePlayer(UnturnedPlayer player)
-		{
-			if (!player.IsAdmin && !player.HasPermission(Config.ExemptPermission))
-			{
-				CheckQueue.Add(player);
-			}
+			UnturnedPlayerEvents.OnPlayerWear -= OnPlayerWear;
 		}
 
 		private void OnPlayerConnected(UnturnedPlayer player)
 		{
-			QueuePlayer(player);
+			// Wait for player to fully load in.
+			CheckInventory(player);
 		}
 
-		private void OnPlayerInventoryAdded(UnturnedPlayer player, Rocket.Unturned.Enumerations.InventoryGroup inventoryGroup, byte inventoryIndex, ItemJar P)
+		private void OnPlayerWear(UnturnedPlayer player, UnturnedPlayerEvents.Wearables wear, ushort id, byte? quality)
 		{
-			// You can't just remove the item here because that can cause issues.
-			QueuePlayer(player);
-		}
-
-		private void OnPlayerDisconnected(UnturnedPlayer player)
-		{
-			CheckQueue.Remove(player);
-		}
-
-		public void Update()
-		{
-			if (Time.time - LastCheck > Config.CheckInterval)
+			if (!CanUseItem(player, id))
 			{
-				LastCheck = Time.time;
-				foreach (var QueuedPlayer in CheckQueue)
+				new Thread(() =>
 				{
-					CheckInventory(QueuedPlayer);
-					CheckQueue.Remove(QueuedPlayer);
-				}
+					Thread.CurrentThread.IsBackground = true;
+					// Delay is necessary, if you unequip immediately
+					// the item will not get unequipped at all.
+					Thread.Sleep(2000);
+					try
+					{
+						switch (wear)
+						{
+							case UnturnedPlayerEvents.Wearables.Hat:
+								player.Player.clothing.askWearHat(0, 0, new byte[0], true);
+								break;
+							case UnturnedPlayerEvents.Wearables.Mask:
+								player.Player.clothing.askWearMask(0, 0, new byte[0], true);
+								break;
+							case UnturnedPlayerEvents.Wearables.Vest:
+								player.Player.clothing.askWearVest(0, 0, new byte[0], true);
+								break;
+							case UnturnedPlayerEvents.Wearables.Pants:
+								player.Player.clothing.askWearPants(0, 0, new byte[0], true);
+								break;
+							case UnturnedPlayerEvents.Wearables.Shirt:
+								player.Player.clothing.askWearShirt(0, 0, new byte[0], true);
+								break;
+							case UnturnedPlayerEvents.Wearables.Glasses:
+								player.Player.clothing.askWearGlasses(0, 0, new byte[0], true);
+								break;
+							case UnturnedPlayerEvents.Wearables.Backpack:
+								player.Player.clothing.askWearBackpack(0, 0, new byte[0], true);
+								break;
+							default:
+								break;
+						}
+					}
+					catch { }
+				}).Start();
 			}
-			//if (Level.isLoaded && Provider.clients.Count > 0)
-			//{
-			//	if (Time.time - LastCheck > Config.CheckInterval)
-			//	{
-			//		LastCheck = Time.time;
-			//		foreach (var client in Provider.clients)
-			//		{
-			//			if (!UnturnedPlayer.FromSteamPlayer(client).IsAdmin && !UnturnedPlayer.FromSteamPlayer(client).HasPermission(Config.ExemptPermission))
-			//			{
-			//				CheckInventory(client.player);
-			//			}
-			//		}
-			//	}
-			//}
+		}
+
+
+		private void OnPlayerInventoryAdded(UnturnedPlayer player, InventoryGroup inventoryGroup, byte inventoryIndex, ItemJar P)
+		{
+			if (!CanUseItem(player, P.item.id))
+			{
+				if (player.Player.equipment.itemID == P.item.id)
+				{
+					player.Player.equipment.dequip();
+				}
+				new Thread(() =>
+				{
+					Thread.CurrentThread.IsBackground = true;
+					// So this delay is needed between dequipping the
+					// equipped tool and removing it to reduce weird errors.
+					// It also seems to reduce weird graphical glitches
+					// in the inventory when removing stuff;
+					Thread.Sleep(2000);
+					try
+					{
+						// Oddly enough the inventoryIndex is less reliable than the getIndex version.
+						player.Player.inventory.removeItem(Convert.ToByte(inventoryGroup), player.Player.inventory.getIndex(Convert.ToByte(inventoryGroup), P.x, P.y));
+						UnturnedChat.Say(player, Translate("restricteditem_removed", UnturnedItems.GetItemAssetById(P.item.id).itemName), Color.red);
+					}
+					catch { }
+				}).Start();
+			}
 		}
 
 		public void CheckInventory(UnturnedPlayer uPlayer)
 		{
 			var player = uPlayer.Player;
-			try
+			
+			if (!CanUseItem(uPlayer, player.clothing.backpack))
 			{
-				// This might have issues if the clothing item isn't there or something.
-				if (!CanUseItem(uPlayer, player.clothing.backpack))
+				player.clothing.askWearBackpack(0, 0, new byte[0], true);
+			}
+			if (!CanUseItem(uPlayer, player.clothing.vest))
+			{
+				player.clothing.askWearVest(0, 0, new byte[0], true);
+			}
+			if (!CanUseItem(uPlayer, player.clothing.shirt))
+			{
+				player.clothing.askWearShirt(0, 0, new byte[0], true);
+			}
+			if (!CanUseItem(uPlayer, player.clothing.pants))
+			{
+				player.clothing.askWearPants(0, 0, new byte[0], true);
+			}
+			if (!CanUseItem(uPlayer, player.clothing.hat))
+			{
+				player.clothing.askWearHat(0, 0, new byte[0], true);
+			}
+			if (!CanUseItem(uPlayer, player.clothing.mask))
+			{
+				player.clothing.askWearMask(0, 0, new byte[0], true);
+			}
+			if (!CanUseItem(uPlayer, player.clothing.glasses))
+			{
+				player.clothing.askWearGlasses(0, 0, new byte[0], true);
+			}
+			
+			for (byte page = 0; page < PlayerInventory.PAGES-1; page++)
+			{
+				// This should check for if the page exists.
+				for (byte index = player.inventory.getItemCount(page); index-- > 0;)
 				{
-					player.clothing.askWearBackpack(0, 0, new byte[0], true);
-				}
-				if (!CanUseItem(uPlayer, player.clothing.vest))
-				{
-					player.clothing.askWearVest(0, 0, new byte[0], true);
-				}
-				if (!CanUseItem(uPlayer, player.clothing.shirt))
-				{
-					player.clothing.askWearShirt(0, 0, new byte[0], true);
-				}
-				if (!CanUseItem(uPlayer, player.clothing.pants))
-				{
-					player.clothing.askWearPants(0, 0, new byte[0], true);
-				}
-				if (!CanUseItem(uPlayer, player.clothing.hat))
-				{
-					player.clothing.askWearHat(0, 0, new byte[0], true);
-				}
-				if (!CanUseItem(uPlayer, player.clothing.mask))
-				{
-					player.clothing.askWearMask(0, 0, new byte[0], true);
-				}
-				if (!CanUseItem(uPlayer, player.clothing.glasses))
-				{
-					player.clothing.askWearGlasses(0, 0, new byte[0], true);
-				}
-
-				// This pretty much always causes a nullpointer or something.
-				for (byte page = 0; page < PlayerInventory.PAGES; page++)
-				{
-					for (byte index = 0; index < player.inventory.getItemCount(page); index++)
+					if (!CanUseItem(uPlayer, player.inventory.getItem(page, index).item.id))
 					{
-						if (!CanUseItem(uPlayer, player.inventory.getItem(page, index).item.id))
-						{
-							UnturnedChat.Say(uPlayer, Translate("restricteditem_removed", UnturnedItems.GetItemAssetById(player.inventory.getItem(page, index).item.id).itemName), Color.red);
-							player.inventory.removeItem(page, index);
-						}
+						UnturnedChat.Say(uPlayer, Translate("restricteditem_removed", UnturnedItems.GetItemAssetById(player.inventory.getItem(page, index).item.id).itemName), Color.red);
+						player.inventory.removeItem(page, index);
 					}
 				}
 			}
-			catch { }
 		}
 
 		public bool CanUseItem(UnturnedPlayer player, ushort ID)
 		{
-			if (IsRestrictedItem(ID))
+			if (player.IsAdmin || player.HasPermission(Config.ExemptPermission))
 			{
-				if (!AllRestrictedItems.ContainsKey(ID))
+				return true;
+			}
+			
+			if (ID == 0)
+			{
+				return true;
+			}
+
+			if (AllRestrictedItems.ContainsKey(ID))
+			{
+				foreach (var PermissionGroup in AllRestrictedItems[ID])
 				{
-					return false;
-				}
-				else
-				{
-					foreach (var PermissionGroup in AllRestrictedItems[ID])
+					// I tried to use HasPermissions, but that seemed to
+					// return true if one of the perms was true, not all.
+					var hasPerm = true;
+					foreach (var Permission in PermissionGroup)
 					{
-						// I tried to use HasPermissions, but that seemed to
-						// return true if one of the perms was true, not all.
-						var hasPerm = true;
-						foreach (var Permission in PermissionGroup)
+						if (!player.HasPermission(Permission))
 						{
-							if (!player.HasPermission(Permission))
-							{
-								hasPerm = false;
-							}
-						}
-						if (hasPerm)
-						{
-							return true;
+							hasPerm = false;
 						}
 					}
+					if (hasPerm)
+					{
+						return true;
+					}
 				}
+				return false;
 			}
 			else
 			{
-				return true;
+				if (Config.UnlistedAreRestricted)
+				{
+					return false;
+				}
 			}
-			return false;
-		}
 
-		public bool IsRestrictedItem(ushort ID)
-		{
-			if (ID == 0)
-			{
-				return false;
-			}
-			if (Config.UnlistedAreRestricted)
-			{
-				return true;
-			}
-			if (AllRestrictedItems.ContainsKey(ID))
-			{
-				return true;
-			}
-			return false;	
+			return true;
 		}
 
 		public override TranslationList DefaultTranslations
